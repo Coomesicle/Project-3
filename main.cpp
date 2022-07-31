1a6b44a2b47e574e69b7537d349bc49a21771a72
Coomesicle <stephencoomes@ufl.edu>
2022-07-30 20:51:53 -0400

    Added Quick Sort
    for title and price

--- main.cpp
+++ main.cpp
@@ -1,4 +1,74 @@
 #include "VideoGame.h"
+#include <iostream>
+
+void swap(std::vector<VideoGame>& v,int a,int b){
+	VideoGame temp = v.at(a);
+	v.at(a)=v.at(b);
+	v.at(b)=temp;
+}
+int PriceSort(std::vector<VideoGame>& v, int low, int high){
+	int pivot = v.at(low).getPrice();
+	int up = low;
+	int down = high;
+	
+	while(up < down){
+		for(int j = up; j < high; j++){
+			if(v.at(up).getPrice() > pivot){
+				break;
+			}
+			up++;
+		}
+		for(int j = high; j > low; j--){
+			if(v.at(down).getPrice() < pivot){
+				break;
+			}
+			down--;
+		}
+		if(up<down){
+			swap(v,up,down);
+		}
+		swap(v,low,down);
+	}
+}
+int TitleSort(std::vector<VideoGame>& v, int low, int high){
+	std::string pivot = v.at(low).getTitle();
+	int up = low;
+	int down = high;
+	
+	while(up < down){
+		for(int j = up; j < high; j++){
+			if(v.at(up).getTitle() > pivot){
+				break;
+			}
+			up++;
+		}
+		for(int j = high; j > low; j--){
+			if(v.at(down).getTitle() < pivot){
+				break;
+			}
+			down--;
+		}
+		if(up<down){
+			swap(v,up,down);
+		}
+		swap(v,low,down);
+	}
+}
+void QuickSortPrice(std::vector<VideoGame>& v, int low, int high){
+	if(low < high){
+		int pivot = PriceSort(v,low,high);
+		QuickSortPrice(v,low,pivot-1);
+		QuickSortPrice(v,pivot+1,high);
+	}
+}
+void QuickSortTitle(std::vector<VideoGame>& v, int low, int high){
+	if(low < high){
+		int pivot = TitleSort(v,low,high);
+		QuickSortTitle(v,low,pivot-1);
+		QuickSortTitle(v,pivot+1,high);
+	}
+}
+
 
 void mergeTitle(std::vector<VideoGame>& v, int left, int mid, int right){
   int v1 = left;
